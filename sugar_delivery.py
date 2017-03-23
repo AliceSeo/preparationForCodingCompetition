1326f96a15c4a364cded57a923dab595712ba5ba
Alice Seo <jseo129@aucklanduni.ac.nz>
2017-03-23 16:14:42 +1300

    Update sugar_delivery.py

--- sugar_delivery.py
+++ sugar_delivery.py
@@ -1,4 +1,5 @@
 # Title: ŠEĆER
+# Retrieved from Croatian Open Competition in Informatics > COCI 2010/2011 > Contest #7
 # Q.
 # Mirko works in a sugar factory as a delivery boy. He has just received an order: he has to deliver exactly N kilograms
 # of sugar to a candy store on the Adriatic coast. Mirko can use two types of packages, the ones that contain 3 kilograms,
